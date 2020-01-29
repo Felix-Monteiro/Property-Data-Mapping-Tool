ed1ef1d73a1dd9ec8badceeade5a8c6b6fda0cad
Felix Saraiva <45005285+ThunderFelix@users.noreply.github.com>
2020-01-29 16:59:38 +0000

    Creted a dynamic pin list
    Pin list was created dynamically also added a button that delets the pins from the database. (Delete Feature)

--- scripts/index.js
+++ scripts/index.js
@@ -5,7 +5,37 @@ document.addEventListener('DOMContentLoaded', function() {
     var items = document.querySelectorAll('.collapsible');
     M.Collapsible.init(items);
   });
+  
+// set up pin list
+const pinList = document.querySelector('#pin-list');
+const form = document.querySelector('#add-pin-form');
+// create element & render pin
+function renderPin(doc){
+    let li = document.createElement('li');
+    let name = document.createElement('span');
+    let Info = document.createElement('span');
+    let coOrds = document.createElement('span');
+    let cross = document.createElement('div');
+    
+    li.setAttribute('data-id', doc.id);
+    name.textContent = doc.data().name;
+    Info.textContent = doc.data().Info;
+    coOrds.textContent = doc.data().coOrds;
+    cross.textContent = 'X';
 
+    li.appendChild(name);
+    li.appendChild(Info);
+    li.appendChild(coOrds);
+    li.appendChild(cross);
+    pinList.appendChild(li);
+
+// deelte event
+    cross.addEventListener('click', (e) => {
+        e.stopPropagation();
+        let id = e.target.parentElement.getAttribute('data-id');
+        db.collection('Pins').doc(id).delete();
+    });
+}
 
 
 var mymap = L.map('mapid').setView([51.458179,-0.114981], 13);// init with lat lon zoom
