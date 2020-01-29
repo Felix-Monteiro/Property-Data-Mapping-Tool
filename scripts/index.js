de370c151ad44ec8422897895655e5acd70f7bd7
Stprry <stprry96@gmail.com>
2020-01-29 16:07:41 +0000

    map commit

---
+++ scripts/index.js
@@ -0,0 +1,10 @@
+var mymap = L.map('mapid').setView([51.458179,-0.114981], 13);// init with lat lon zoom
+
+
+L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    id: 'mapbox/streets-v11',
+    accessToken: 'pk.eyJ1IjoidGVzdGFjYzEiLCJhIjoiY2s1Z3N6djl5MDkweDNubXBwbHc5bGhiNCJ9.TUdGQk7n0swbE-bnEOy26A'
+}).addTo(mymap);
+//(51.456146, -0.108147)
